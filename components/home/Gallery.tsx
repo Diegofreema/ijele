2af5c002df9c5872ea25bb088fc065152a5ba515
Diego <diegofreeman78@gmail.com>
2024-05-30 18:57:15 +0100

    ui changed

--- components/home/Gallery.tsx
+++ components/home/Gallery.tsx
@@ -13,6 +13,7 @@ import { LightContainer } from '../ui/LightContainer';
 import { motion } from 'framer-motion';
 import { ImageType } from '@/types';
 import { useMemo } from 'react';
+import { MyText } from '../ui/MyText';
 
 interface Props {
   images: ImageType[];
@@ -53,39 +54,44 @@ export const Gallery = ({ images }: Props) => {
           </Link>
         )}
       </Flex>
-      <Grid
-        templateColumns={{ base: '1fr', md: '1fr 1fr 1fr' }}
-        templateRows="1fr"
-        gap={10}
-        maxWidth={{ base: '90%', md: '80%' }}
-        mx={'auto'}
-      >
-        {memoImages?.map((item, i) => (
-          <GridItem
-            key={i}
-            as={motion.div}
-            initial={{ x: i === 0 || i === 3 ? -50 : 50, opacity: 0 }}
-            whileInView={{
-              x: 0,
-              opacity: 1,
-              transition: { delay: 0.3, duration: 0.3 },
-            }}
-            viewport={{ once: true }}
-            rowSpan={1}
-            height={'300px'}
-            colSpan={{ base: 1, md: i === 0 || i === 3 ? 1 : 2 }}
-          >
-            <Image
-              alt="image"
-              src={item?.image_url as string}
-              width={'100%'}
-              height={'100%'}
-              objectFit={'cover'}
-              borderRadius={10}
-            />
-          </GridItem>
-        ))}
-      </Grid>
+      {memoImages?.length > 0 && (
+        <Grid
+          templateColumns={{ base: '1fr', md: '1fr 1fr 1fr' }}
+          templateRows="1fr"
+          gap={10}
+          maxWidth={{ base: '90%', md: '80%' }}
+          mx={'auto'}
+        >
+          {memoImages?.map((item, i) => (
+            <GridItem
+              key={i}
+              as={motion.div}
+              initial={{ x: i === 0 || i === 3 ? -50 : 50, opacity: 0 }}
+              whileInView={{
+                x: 0,
+                opacity: 1,
+                transition: { delay: 0.3, duration: 0.3 },
+              }}
+              viewport={{ once: true }}
+              rowSpan={1}
+              height={'300px'}
+              colSpan={{ base: 1, md: i === 0 || i === 3 ? 1 : 2 }}
+            >
+              <Image
+                alt="image"
+                src={item?.image_url as string}
+                width={'100%'}
+                height={'100%'}
+                objectFit={'cover'}
+                borderRadius={10}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      )}
+      {memoImages?.length === 0 && (
+        <MyText text="No images yet" textAlign={'center'} />
+      )}
     </LightContainer>
   );
 };
